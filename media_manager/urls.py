933691e8176105f3fe9590775d8434436c11c68a
Hariom Dubey <hariom.dubey@grayquest.com>
2022-07-31 11:51:39 +0530

    testing github webhook 2

--- media_manager/urls.py
+++ media_manager/urls.py
@@ -12,7 +12,8 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-    3. Test
+    3. Test 1
+    4. Test 2
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
