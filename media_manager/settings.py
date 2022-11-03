82729aac9e906cecbb85f40117b59e0e2c5b34ea
Hariom Dubey <hariom.dubey@grayquest.com>
2022-11-03 07:42:56 +0530

    updated allowed hosts

--- media_manager/settings.py
+++ media_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'b!de&a8wvn*x-ds^)$q9&3foh49jozigaq14o4=x@e2l&-mj-d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.91.0.169', 'ec2-52-91-0-169.compute-1.amazonaws.com']
+ALLOWED_HOSTS = ['3.85.221.111', 'ec2-3-85-221-111.compute-1.amazonaws.com']
 
 
 # Application definition
