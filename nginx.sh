f8187b2e4c2ecf3e3601ab9da002b31f5c376b33
Hariom Dubey <hariom.dubey@grayquest.com>
2022-11-04 09:00:26 +0530

    updated nginx file13

--- nginx.sh
+++ nginx.sh
@@ -1,2 +1,15 @@
-#! bin/bash
+#! /bin/bash
 
+echo 'Nginx Running Directory'
+echo $PWD
+
+sudo nginx -t
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+sudo systemctl restart nginx
+
+echo "Nginx status"
+
+sudo systemctl status nginx
