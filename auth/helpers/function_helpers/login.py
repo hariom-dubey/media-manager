f253f2f0c2bd2697d23c114cf997c551a8349c31
hariom <dubeyhariom2020@gmail.com>
2023-01-29 20:22:03 +0530

    full details

--- auth/helpers/function_helpers/login.py
+++ auth/helpers/function_helpers/login.py
@@ -56,7 +56,8 @@ def get_client_ip_address(request):
         req_headers = request.META
         x_forwarded_for_value = req_headers.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for_value:
-            ip_addr = x_forwarded_for_value.split(',')[-1].strip()
+            # ip_addr = x_forwarded_for_value.split(',')[-1].strip()
+            ip_addr = x_forwarded_for_value
         else:
             ip_addr = req_headers.get('REMOTE_ADDR', '')
     except:
