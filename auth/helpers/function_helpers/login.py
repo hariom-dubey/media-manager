20dfed2c97e0603a8df69ee803d368ec3cfe7a4d
hariom <dubeyhariom2020@gmail.com>
2023-01-29 20:25:33 +0530

    ip config test

--- auth/helpers/function_helpers/login.py
+++ auth/helpers/function_helpers/login.py
@@ -21,7 +21,7 @@ def verify_login_v1(request):
             return Response({
                     'success': False,
                     'status_code': status.HTTP_400_BAD_REQUEST,
-                    'message':  'User Not Found',
+                    'message':  'User Not Found !',
                     'data': {'ip_addr': ip_addr}
                 },
                 status=status.HTTP_400_BAD_REQUEST
@@ -56,8 +56,7 @@ def get_client_ip_address(request):
         req_headers = request.META
         x_forwarded_for_value = req_headers.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for_value:
-            # ip_addr = x_forwarded_for_value.split(',')[-1].strip()
-            ip_addr = x_forwarded_for_value
+            ip_addr = x_forwarded_for_value.split(',')[-1].strip()
         else:
             ip_addr = req_headers.get('REMOTE_ADDR', '')
     except:
