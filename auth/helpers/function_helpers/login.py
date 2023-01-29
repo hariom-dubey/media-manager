38b493b3d51582dfedb19026e3810753e6c7d484
hariom <dubeyhariom2020@gmail.com>
2023-01-29 19:43:37 +0530

    feat(IP): adter login it will return the ip address in data

--- auth/helpers/function_helpers/login.py
+++ auth/helpers/function_helpers/login.py
@@ -15,12 +15,14 @@ def verify_login_v1(request):
 
         user = fetch_user_details_v1(email)
 
+        ip_addr = get_client_ip_address(request)
+
         if not user:
             return Response({
                     'success': False,
                     'status_code': status.HTTP_400_BAD_REQUEST,
                     'message':  'User Not Found',
-                    'data': None
+                    'data': {'ip_addr': ip_addr}
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
@@ -32,7 +34,7 @@ def verify_login_v1(request):
                     'success': True,
                     'status_code': status.HTTP_200_OK,
                     'message':  'Login Successfull !',
-                    'data': None
+                    'data': {'ip_addr': ip_addr}
                 },
                 status=status.HTTP_200_OK
             )
@@ -41,10 +43,23 @@ def verify_login_v1(request):
                     'success': False,
                     'status_code': status.HTTP_401_UNAUTHORIZED,
                     'message':  'Incorrect Password',
-                    'data': None
+                    'data': {'ip_addr': ip_addr}
                 },
                 status=status.HTTP_401_UNAUTHORIZED
             )
 
     except Exception as e:
         raise ce.InternalServerError
+
+def get_client_ip_address(request):
+    try:
+        req_headers = request.META
+        x_forwarded_for_value = req_headers.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for_value:
+            ip_addr = x_forwarded_for_value.split(',')[-1].strip()
+        else:
+            ip_addr = req_headers.get('REMOTE_ADDR', '')
+    except:
+        ip_addr = ''
+        
+    return ip_addr
